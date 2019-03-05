35b6ac077511284b3e7a53716b81b33b6d950c79
apclarkva <apclarkva@gmail.com>
2019-03-05 09:51:35 -0500

    update

--- run_ap_data_2.sh
+++ run_ap_data_2.sh
@@ -2,30 +2,23 @@
 
 CURRI=$1
  
-# #SBATCH --partition=panda   # cluster-specific
-# #SBATCH --nodes=6
-# #SBATCH --ntasks=200
-# #SBATCH --job-name=christini-job
-# #SBATCH --time=04:00:00   # HH/MM/SS
-# #SBATCH --mem=1G
+#SBATCH --partition=panda   # cluster-specific
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=christini-job
+#SBATCH --time=04:00:00   # HH/MM/SS
+#SBATCH --mem=1G
  
-# source ~/.bashrc
+source ~/.bashrc
  
-# echo "Starting at:" `date` >> population_output.txt
-# sleep 30
-# echo "This is job #:" $SLURM_JOB_ID >> population_output.txt
-# echo "Running on node:" `hostname` >> population_output.txt
-# echo "Running on cluster:" $SLURM_CLUSTER_NAME >> population_output.txt
-# echo "This job was assigned the temporary (local) directory:" $TMPDIR >> population_output.txt
+echo "Starting at:" `date` >> population_output.txt
+sleep 30
+echo "This is job #:" $SLURM_JOB_ID >> population_output.txt
+echo "Running on node:" `hostname` >> population_output.txt
+echo "Running on cluster:" $SLURM_CLUSTER_NAME >> population_output.txt
+echo "This job was assigned the temporary (local) directory:" $TMPDIR >> population_output.txt
 
-# /softlib/exe/x86_64/pkg/matlab/2018b/bin/matlab -r "run('main_ventricular.m')"
+# sbatch --array=1-40001 script_name.sh
+/softlib/exe/x86_64/pkg/matlab/2018b/bin/matlab -r "main_vent(${SLURM_ARRAY_TASK_ID})"
  
-# exit
-
-printf $CURRI
-
-# /softlib/exe/x86_64/pkg/matlab/2018b/bin/matlab -r "run('test()')"
-
-# /softlib/exe/x86_64/pkg/matlab/2018b/bin/matlab -r "test(${CURRI})"
-
-/Applications/MATLAB_R2018b.app/bin/matlab -nodisplay -nosplash -nodesktop -r "main_vent(${CURRI})"
+exit
